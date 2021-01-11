d4b109f4278d5828ff22d5db2316860d602b400a
Alex <agasparr98@gmail.com>
2021-01-11 11:57:54 +0100

    exo fini

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+// # Exercice 1 boucle while
+// - A l'aide d'un prompt, choisissez un chiffre et enregistrez la valeur dans une variable
+// - Déclarez une variable compteur qui commence par 0
+// - A l'aide d'une boucle while, incrémentez de 1 la variable compteur à chaque boucle et affichez touts les chiffres de 0 jusqu'au nombre choisis au debut dans le prompt 
+
+// let chiffre = parseInt(prompt('Chiffre?'));
+// let zero = 0
+
+// while (zero <= chiffre) {
+//     console.log(zero);
+//     zero++
+// }
+
+
+
+// # Exercice 2 boucle WHILE
+// 1. Créer une boucle while qui affiche un console log avec le numéro du tour 
+// 2. Créer une boucle while qui affiche un console log avec le numéro du tour en sachant que le tour doit commencer à 3 et s'arreter à 6
+// 3. Créer une boucle qui affiche le numéro et qui s'incrémente de 2
+
+let tour = 3
+
+while (tour <= 6) {
+    console.log(tour);
+    tour+=2
+}
